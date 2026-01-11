679bceb64413de8cfe7018077cecde0824e8417b
syedkhizar123 <khizarrehman2007@gmail.com>
2026-01-11 21:36:37 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const connectDB = require('./config/DB')
+const connectDB = require('./config/db')
 dotenv.config({quiet: true})
 const PORT = process.env.PORT || 3000
 const app = express()
