c0718c9ac18a405a40b19da4c14e0c946bf49b0f
syedkhizar123 <khizarrehman2007@gmail.com>
2026-01-12 12:11:26 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,30 +1,16 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-dotenv.config({ quiet: true });
-
-const MONGODB_URL = process.env.MONGODB_URL;
-
-let isConnected = false;
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config({quiet : true})
+const MONGODB_URL = process.env.MONGODB_URL
 
 const connectDB = async () => {
-    if (isConnected) {
-        return;
-    }
-
-    try {
-        mongoose.set('bufferCommands', false); 
-
-        const conn = await mongoose.connect(MONGODB_URL, {
-            serverSelectionTimeoutMS: 5000, 
-        });
-
-        isConnected = conn.connections[0].readyState;
-        console.log('MongoDB connected');
+    try{
+        const connect = await mongoose.connect(MONGODB_URL)
+        console.log("MongoDB connected:");
     } catch (error) {
-        console.error('MongoDB connection error:', error.message);
-        throw error; 
-    }
-};
+        console.log(error)   
+    } 
+}
 
 module.exports = connectDB;
+
